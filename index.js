3376ae2d6e271b01ba13a53947bad11fbc66adb9
rududureda <ranc.reda@gmail.com>
2024-03-21 22:17:16 +0200

    add vercel

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 require('dotenv').config();
 
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 const productGetterRoutes = require('./routes/productGetterRoutes');
 const productSetterRoutes = require('./routes/productSetterRoutes');
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
 
 app.use('/product', productGetterRoutes);
 app.use('/product', productSetterRoutes);
